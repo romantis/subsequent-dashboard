58b3daf3d103d47ec0a1b754f5e880e67ff8cee1
Roman Tyshchenko <udatos@gmail.com>
2024-05-06 18:22:58 +0300

    feat: implement invoices CRUD

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +18,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -108,6 +110,7 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        <input type="hidden" name="id" value={invoice.id} />
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
